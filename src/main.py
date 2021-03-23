e08730409733e2bd5b9f0e7c20dc4e5d892e1f07
Liam <71791017+llalon@users.noreply.github.com>
2021-03-23 17:43:37 -0400

    user scripts

--- src/main.py
+++ src/main.py
@@ -6,12 +6,12 @@ import func
 
 # Command delcarations
 commands = {
-    'help': func.show_help,
-    'request': func.request,
-    'invite': func.invite,
-    'issue': func.send_feedback,
-    'search': func.search,
-    'top': func.leader_board
+    "help": func.show_help,
+    "request": func.request,
+    "invite": func.invite,
+    "issue": func.send_feedback,
+    "search": func.search,
+    "top": func.leader_board,
 }
 
 client = discord.Client()
@@ -19,7 +19,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
 
 
 @client.event
@@ -29,12 +29,20 @@ async def on_message(message):
 
     # Parse commands
     for c in commands:
-        if (message.content.startswith('!' + c)):
+        if message.content.startswith("!" + c):
             msg = commands[c](message)
             await message.channel.send(msg)
 
 
 def init_anna():
+
+    # Find find scripts
+    dir = r"{}".format(settings.SCR_DIR)
+    for script in os.listdir(dir):
+        if script.endswith(".sh"):
+            print(os.path.join(directory, filename))
+
+    # Start the bot
     client.run(settings.DISCORD_TOKEN)
 
 
